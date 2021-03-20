14cf75263f8b38c5efec153f71b94086f70bf67a
VetalosUA-KR <vetal925@gmail.com>
2021-03-20 15:42:13 +0200

    added enemy movement

--- core/src/com/mygdx/game/EnemyShip.java
+++ core/src/com/mygdx/game/EnemyShip.java
@@ -2,13 +2,41 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class EnemyShip extends Ship {
+
+    Vector2 directionVector;
+    float timeSinceLastDirectionChange = 0;
+    float directionChangeFrequency = 0.75f;
 
     public EnemyShip(float xCentre, float yCentre, float width, float height, float movementSpeed, int shield,
                      float laserWidth, float laserHeight, float laserMovementSpeed, float timeBetweenShots,
                      TextureRegion shipTextureRegion, TextureRegion shieldTextureRegion, TextureRegion laserTextureRegion) {
         super(xCentre, yCentre, width, height, movementSpeed, shield, laserWidth, laserHeight, laserMovementSpeed, timeBetweenShots, shipTextureRegion, shieldTextureRegion, laserTextureRegion);
+
+        directionVector = new Vector2(0, -1);
+
+    }
+
+    public Vector2 getDirectionVector() {
+        return directionVector;
+    }
+
+    private void randomizeDirectionVector() {
+        double bearing = SpaceShooterGame.random.nextDouble() * 6.283185;
+        directionVector.x = (float) Math.sin(bearing);
+        directionVector.y = (float) Math.cos(bearing);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        timeSinceLastDirectionChange += deltaTime;
+        if (timeSinceLastDirectionChange > directionChangeFrequency) {
+            randomizeDirectionVector();
+            timeSinceLastDirectionChange -= directionChangeFrequency;
+        }
     }
 
     @Override
